9b1b978f1b99f5ab693f781ef4ee8adb4cccf683
Andrey Platonov <one.and.pl@gmail.com>
2025-09-08 19:38:05 +0300

    fix

--- python-basics/task_2.py
+++ python-basics/task_2.py
@@ -1,9 +1,9 @@
 # вводим значение года
 
-year_check=int(input('Введите год: '))
+year_check = int(input('Введите год: '))
 
 # условие проверки на високосный год
-check_year=(year_check % 400 ==0 or (year_check % 4 ==0 and year_check % 100 >0))
+check_year = (year_check % 400 ==0 or (year_check % 4 ==0 and year_check % 100 > 0))
 
 if check_year:
   print('Високосный год')
